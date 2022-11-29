51916686a9b62d0f0591e26ee77b7307976d48f0
fluorostani <glyn.dimond@artos.io>
2022-11-29 21:15:08 +0000

    SYS-2921 Fix requiredConfirmations calculation

--- test/AVN_gas.js
+++ test/AVN_gas.js
@@ -8,11 +8,11 @@ const allowedGas = {
   erc777ProxyLower: 87700,
   erc20ProxyLower: 76650,
   ethProxyLower: 66050,
-  publishRoot: 127200,
-  triggerGrowth: 149500,
+  publishRoot: 127450,
+  triggerGrowth: 149700,
   releaseGrowth: 39100,
   triggerGrowth_via_owner: 71650,
-  triggerGrowth_immediate_release: 150500,
+  triggerGrowth_immediate_release: 150900,
   updateLowerCall: 45800,
   transferOwnership: 31850
 }
