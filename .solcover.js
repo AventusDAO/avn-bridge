69f22f9c6eb2944ca53bfdb0ed03814100418ce3
fluorostani <glyn.dimond@artos.io>
2023-03-16 12:32:37 +0000

    remove artifacts

--- .solcover.js
+++ .solcover.js
@@ -1,10 +1,4 @@
 module.exports = {
-  client: require('ganache-cli'),
-  providerOptions: {
-    total_accounts: 30,
-    account_keys_path: 'keys.json',
-    mnemonic: 'lady sad two vacuum rail siren barrel convince rare helmet wagon approve'
-  },
   skipFiles: [
     'test/Token20.sol',
     'test/Token777.sol',
