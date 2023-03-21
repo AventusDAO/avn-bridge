b3271bcd851dc4499c5df02db227eaa5f31cbc06
fluorostani <39763263+fluorostani@users.noreply.github.com>
2023-03-21 16:06:22 +0000

    SYS-3203 Enable better integration with avn-scripts (#55)

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,13 +7,15 @@ require('hardhat-gas-reporter');
 require('solidity-coverage');
 require('hardhat-erc1820');
 
-const { INFURA_API_KEY, ETHERSCAN_API_KEY, GOERLI_PRIVATE_KEY, MAINNET_PRIVATE_KEY } = require('./config')
+const { INFURA_API_KEY, ETHERSCAN_API_KEY, GOERLI_PRIVATE_KEY, MAINNET_PRIVATE_KEY } = require('./config');
+const fs = require('fs');
 
-task('loadValidators', 'populate an avn-bridge contract with a set of validators')
-  .addParam('address', 'address of target avn-bridge contract')
-  .addParam('file', 'path to the validators file')
+task('loadValidators', 'initialise a new avn-bridge contract with a set of validators')
+  .addParam('contract', 'avn-bridge contract address')
+  .addParam('validators', 'path to validators file')
   .setAction(async (args) => {
-    const validators = require(args.file);
+    console.log(`\nLoading validators from ${args.validators} into avn-bridge @ ${args.contract}`);
+    const validators = require(args.validators);
     const t1Address = [], t1PublicKeyLHS = [], t1PublicKeyRHS = [], t2PublicKey = [];
 
     validators.forEach(validator => {
@@ -23,8 +25,53 @@ task('loadValidators', 'populate an avn-bridge contract with a set of validators
       t2PublicKey.push(validator.validator.tier2PublicKeyHex);
     });
 
-    const avnBridge = await ethers.getContractAt('contracts/AVNBridge.sol:AVNBridge', args.address);
+    const avnBridge = await ethers.getContractAt('contracts/AVNBridge.sol:AVNBridge', args.contract);
     await avnBridge.loadValidators(t1Address, t1PublicKeyLHS, t1PublicKeyRHS, t2PublicKey);
+  });
+
+task('deploy', 'deploy a new avn-bridge contract and (optionally) initialise with validators')
+  .addOptionalParam('token', 'optional core token address (eg: AVT contract)', '0xe0A9E4f2591be648f18001e21dB16dDAB114fEF9')
+  .addOptionalParam('validators', 'optional path to file containing any validators to be loaded')
+  .setAction(async (args, hre) => {
+    await hre.run('compile');
+
+    if (hre.network.name === 'mainnet') {
+      console.log('Requires manual setup for mainnet deployment');
+      process.exit(1);
+    } else {
+      const [deployer] = await hre.ethers.getSigners();
+      console.log(`\nDeploying to ${hre.network.name} network using account ${deployer.address}...`);
+
+      // delete any existing OZ manifests as we want to deploy anew, not upgrade
+      if (fs.existsSync('./.openzeppelin/goerli.json')) fs.unlinkSync('./.openzeppelin/goerli.json');
+      if (fs.existsSync('./.openzeppelin/unknown-73799.json')) fs.unlinkSync('./.openzeppelin/unknown-73799.json');
+
+      const balanceBefore = await deployer.getBalance();
+      const AVNBridge = await hre.ethers.getContractFactory('AVNBridge');
+      const avnBridge = await hre.upgrades.deployProxy(AVNBridge, [args.token, '0x0000000000000000000000000000000000000000'], { kind: 'uups' });
+      await avnBridge.deployed();
+      const implementationAddress = await hre.upgrades.erc1967.getImplementationAddress(avnBridge.address);
+
+      if (network.name === 'goerli') { // publish contracts to Etherscan
+        await new Promise((r) => setTimeout(r, 20000));
+        await hre.run('verify', { address: implementationAddress });
+        await hre.run('verify', { address: avnBridge.address });
+      }
+
+      console.log(`\nTotal cost: ${hre.ethers.utils.formatEther(balanceBefore.sub(await deployer.getBalance()))} ETH`);
+      console.log(`\nContract: ${avnBridge.address}`);
+
+      if (args.validators) { // run optional loadValidators task
+        await hre.run('loadValidators', { contract: avnBridge.address, validators: args.validators });
+      }
+
+      // output new contract address to file
+      const outFile = './addresses.json';
+      const addresses = fs.existsSync(outFile) ? require(outFile) : {};
+      addresses[hre.network.name] = avnBridge.address;
+      fs.writeFileSync(outFile, JSON.stringify(addresses, null, 2));
+
+    }
   });
 
 module.exports = {
