147808af853528dce5764e8fc36850328721387b
fluorostani <glyn.dimond@artos.io>
2023-09-21 13:31:54 +0100

    Remove skip-on-coverage now tests are fixed

--- .solcover.js
+++ .solcover.js
@@ -1,7 +1,3 @@
 module.exports = {
-  skipFiles: ['test/Token20.sol', 'test/Token777.sol'],
-  mocha: {
-    grep: '@skip-on-coverage',
-    invert: true
-  }
+  skipFiles: ['test/Token20.sol', 'test/Token777.sol']
 };
