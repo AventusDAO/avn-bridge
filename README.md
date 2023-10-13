118c9bdb1f0e61a83aed36f83c93a2fe6e09b7b5
fluorostani <glyn.dimond@artos.io>
2023-10-13 11:29:34 +0100

    SYS-3699 Revert master to a43769c
    This commit contains the contracts as they exist on mainnet presently, along with some minor test case updates that were included after the last release. It will serve as a base to implement the interim growth changes.

--- README.md
+++ README.md
@@ -8,11 +8,11 @@ The system is underwritten by its constructor-specified core token (in the case 
 
 #### The AVN Bridge has 4 main responsibilities:
 
-1. Management of *Authors* - AvN nodes responsible for block creation on T2 and the publishing of their proofs to T1.
+1. Management of *validators* - POS transaction processors existing as actors within the AVN whose token deposits are locked in T2.
 
-2. The periodic checkpointing (*publishing*) by authors of Merkle roots encoding all transactions that have occurred on T2.
+2. The periodic checkpointing (*publishing*) of Merkle tree roots encoding all transactions that have occurred on T2.
 
-3. Securely moving fungible tokens (any token adhering to ERC20 or ERC777 specification) or ETH between T1 Ethereum and the T2 AVN sidechain by the following processes:
+3. Securely moving fungible tokens (any token adhering to ERC20 or ERC777 specification) or ETH between T1 Ethereum mainnet and the T2 AVN sidechain by the following processes:
 - *Lifting* - locking tokens received by T1 and recreating the equivalent amount in the specified T2 recipient account
 - *Lowering* - destroying tokens on T2 and unlocking and transferring the equivalent amount to the specified T1 recipient account
 
@@ -34,33 +34,24 @@ The values in config.json can also be set as environment variables.
 #### Deploy
 `npx hardhat --network <network> deploy --token [optional core token address]`
 
-#### Load a set of authors
-`npx hardhat --network <network> loadAuthors --contract <target contract address> --authors <path to authors json file>`
+#### Load a set of validators
+`npx hardhat --network <network> loadValidators --contract <target contract address> --validators <path to validators json file>`
 
-#### Deploy AND load a set of authors
-`npx hardhat --network <network> deploy --token [optional core token address] --authors <path to authors json file>`
+#### Deploy AND load a set of validators
+`npx hardhat --network <network> deploy --token [optional core token address] --validators <path to validators json file>`
 
 #### Upgrade an existing avn-bridge contract
 `npx hardhat --network <network> upgrade --proxy <proxy contract address>`
 
-#### Force an upgrade (testnet only)
-If the upgrade command fails with: `Error: Deployment at address 0x... is not registered... use the forceImport function` it means your goerli manifest is incorrectly configured. Resolving this requires temporarily swapping out the contracts in order to prepare the correct manifest, before re-attempting the upgrade. Follow these steps:
-
-- Delete the `.openzeppelin/goerli.json` manifest, along with the entire `artifacts` and `cache` folders.
-- Visit the avn-bridge proxy contract address on Goerli etherscan to view the current contract.
-- Click the `Read as Proxy` tab
-- Click on the `Implementation contract` link
-- From its `Code` tab copy both the `AVNBridge.sol` code (at the top of the list of files) and the `IAVNBridge.sol` code (at the bottom) over their respective versions in your local `contracts` directory.
-- Note: If the pragma version at the top of the contracts has changed since (eg: from "`pragma solidity 0.8.21`" to "`pragma solidity 0.8.17`") then the `solidity.compilers.version` value in `hardhat.config.js`'s `module exports` will also require updating to match.
-- Now prepare the new `openzeppelin/goerli.json` manifest by running: `npx hardhat --network goerli prepare-upgrade --proxy <proxy contract address>`
-- You may now revert `AVNBridge.sol` and `IAVNBridge.sol`back to the versions you were originally attempting to upgrade to and run the standard upgrade command again (remembering to reset the solidity compiler version too, if required).
-- **One final note**: dependent upon network conditions, the upgraded contract may not get published and Etherscan will not yet be displaying its updated Read/Write Proxy interface. To correct this, click through the prompts on Etherscan to publish and save the new contract implementation.
+#### Prepare for an upgrade (testnet only)
+If the above upgrade command fails with: `Error: Deployment at address 0x... is not registered... use the forceImport function` you will need to:
+- temporarily replace the AVNBridge.sol contract file you were attempting to deploy with the previous version of that contract
+- prepare the openzeppelin manifest by running `npx hardhat --network <network> prepare-upgrade --proxy <proxy contract address>`
+- now you can revert AVNBridge.sol back to the version you were attempting to upgrade to and run the upgrade command again
 
 #### Publish a new test token
 `npx hardhat --network <network> publishToken`
 
-#### Format the code (JS files only)
-`npm run format`
 
 ### Interaction via Etherscan
 
@@ -73,6 +64,8 @@ The following manual steps are then required to interact with the AVN contract o
 - Return to the ERC1967Proxy page's Contract tab
 - Now you will be able to "Read as Proxy" or "Write as Proxy" to interact with the AVN contract
 
-### Mainnet Address
+### Mainnet Addresses
 
-0xF05Df39f745A240fb133cC4a11E42467FAB10f1F
+AVNBridge Persistent Proxy: 0xF05Df39f745A240fb133cC4a11E42467FAB10f1F
+AVNBridge Version 1 Implementation Address: 0xd0800E6cb9Fe4327BF6e791398f68ab1d76E59a1
+Unlocker: 0x9FC92E791FD6315ab267eC0990D701d81a068c76
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,8 @@
 require('@nomicfoundation/hardhat-toolbox');
 require('@nomicfoundation/hardhat-chai-matchers');
-require('@nomiclabs/hardhat-etherscan');
+require("@nomiclabs/hardhat-etherscan");
 require('@nomiclabs/hardhat-ethers');
-require('@openzeppelin/hardhat-upgrades');
+require("@openzeppelin/hardhat-upgrades");
 require('hardhat-gas-reporter');
 require('solidity-coverage');
 require('hardhat-erc1820');
@@ -10,31 +10,28 @@ require('hardhat-erc1820');
 const { INFURA_API_KEY, ETHERSCAN_API_KEY, GOERLI_PRIVATE_KEY, MAINNET_PRIVATE_KEY } = require('./config');
 const fs = require('fs');
 
-task('loadAuthors', 'initialise a new avn-bridge contract with a set of authors')
+task('loadValidators', 'initialise a new avn-bridge contract with a set of validators')
   .addParam('contract', 'avn-bridge contract address')
-  .addParam('authors', 'path to authors file')
-  .setAction(async args => {
-    console.log(`\nLoading authors from ${args.authors} into avn-bridge @ ${args.contract}`);
-    const authors = require(args.authors);
-    const t1Address = [],
-      t1PublicKeyLHS = [],
-      t1PublicKeyRHS = [],
-      t2PublicKey = [];
+  .addParam('validators', 'path to validators file')
+  .setAction(async (args) => {
+    console.log(`\nLoading validators from ${args.validators} into avn-bridge @ ${args.contract}`);
+    const validators = require(args.validators);
+    const t1Address = [], t1PublicKeyLHS = [], t1PublicKeyRHS = [], t2PublicKey = [];
 
-    authors.forEach(author => {
-      t1Address.push(author.ethAddress);
-      t1PublicKeyLHS.push('0x' + author.ethUncompressedPublicKey.slice(4, 68));
-      t1PublicKeyRHS.push('0x' + author.ethUncompressedPublicKey.slice(68, 132));
-      t2PublicKey.push(author.validator.tier2PublicKeyHex);
+    validators.forEach(validator => {
+      t1Address.push(validator.ethAddress);
+      t1PublicKeyLHS.push('0x' + validator.ethUncompressedPublicKey.slice(4, 68));
+      t1PublicKeyRHS.push('0x' + validator.ethUncompressedPublicKey.slice(68, 132));
+      t2PublicKey.push(validator.validator.tier2PublicKeyHex);
     });
 
     const avnBridge = await ethers.getContractAt('contracts/AVNBridge.sol:AVNBridge', args.contract);
-    await avnBridge.loadAuthors(t1Address, t1PublicKeyLHS, t1PublicKeyRHS, t2PublicKey);
+    await avnBridge.loadValidators(t1Address, t1PublicKeyLHS, t1PublicKeyRHS, t2PublicKey);
   });
 
-task('deploy', 'deploy a new avn-bridge contract and (optionally) initialise with authors')
+task('deploy', 'deploy a new avn-bridge contract and (optionally) initialise with validators')
   .addOptionalParam('token', 'optional core token address (eg: AVT contract)', '0xe0A9E4f2591be648f18001e21dB16dDAB114fEF9')
-  .addOptionalParam('authors', 'optional path to file containing any authors to be loaded')
+  .addOptionalParam('validators', 'optional path to file containing any validators to be loaded')
   .setAction(async (args, hre) => {
     await hre.run('compile');
 
@@ -50,13 +47,12 @@ task('deploy', 'deploy a new avn-bridge contract and (optionally) initialise wit
 
       const balanceBefore = await deployer.getBalance();
       const AVNBridge = await hre.ethers.getContractFactory('AVNBridge');
-      const avnBridge = await hre.upgrades.deployProxy(AVNBridge, [args.token], { kind: 'uups' });
+      const avnBridge = await hre.upgrades.deployProxy(AVNBridge, [args.token, '0x0000000000000000000000000000000000000000'], { kind: 'uups' });
       await avnBridge.deployed();
       const implementationAddress = await hre.upgrades.erc1967.getImplementationAddress(avnBridge.address);
 
-      if (args.authors) {
-        // run optional loadAuthors task
-        await hre.run('loadAuthors', { contract: avnBridge.address, authors: args.authors });
+      if (args.validators) { // run optional loadValidators task
+        await hre.run('loadValidators', { contract: avnBridge.address, validators: args.validators });
       }
 
       // output new contract address to file
@@ -65,24 +61,22 @@ task('deploy', 'deploy a new avn-bridge contract and (optionally) initialise wit
       addresses[hre.network.name]['avn'] = avnBridge.address;
       fs.writeFileSync(outFile, JSON.stringify(addresses, null, 2));
 
-      await new Promise(r => setTimeout(r, 20000));
+      await new Promise((r) => setTimeout(r, 20000));
       try {
         await hre.run('verify', { address: implementationAddress });
         await hre.run('verify', { address: avnBridge.address });
-      } catch (e) {}
+      } catch (e) {};
       console.log(`\nTotal cost: ${hre.ethers.utils.formatEther(balanceBefore.sub(await deployer.getBalance()))} ETH`);
       console.log(`\nContract: ${avnBridge.address}`);
+
     }
   });
 
-task('lift', 'lift a token to the chain')
+  task('lift', 'lift a token to the chain')
   .addParam('recipient', 'Recipient public key (32 bytes) in tier2')
   .addParam('bridge', 'avn-bridge contract address')
   .addParam('amount', 'amount to be lifted')
-  .addOptionalParam(
-    'token',
-    'The contract address of the token to be lifted. If "chain" is passed, it will use the ethereum chain native token. If "core" is passed it will pass the bridge core token'
-  )
+  .addOptionalParam('token', 'The contract address of the token to be lifted. If "chain" is passed, it will use the ethereum chain native token. If "core" is passed it will pass the bridge core token')
   .setAction(async (args, hre) => {
     const bridge = args.bridge;
     const amount = args.amount;
@@ -91,14 +85,16 @@ task('lift', 'lift a token to the chain')
       const avnBridge = await ethers.getContractAt('contracts/AVNBridge.sol:AVNBridge', bridge);
 
       // Handle the case of the chain native token
-      if (args.token === 'chain') {
+      if (args.token === "chain") {
         const liftTx = await avnBridge.liftETH(recipient, { value: amount });
         await liftTx.wait();
         console.log(`Successfully lifted ${amount} of Ether for avn-bridge ${bridge} - lift tx: ${liftTx.hash}`);
         return;
       }
 
-      const token = args.token === undefined || args.token === 'core' ? await avnBridge.coreToken() : args.token;
+      const token = args.token === undefined || args.token === 'core'
+        ? await avnBridge.coreToken()
+        : args.token;
 
       console.log(`\nLifting token ${token} into avn-bridge @ ${bridge}`);
       const tokenContract = await hre.ethers.getContractAt(`ERC20`, token);
@@ -115,28 +111,29 @@ task('lift', 'lift a token to the chain')
     }
   });
 
-task('publishToken', 'deploy a new erc20 test token and publish it').setAction(async (args, hre) => {
-  await hre.run('compile');
+task('publishToken', 'deploy a new erc20 test token and publish it')
+  .setAction(async (args, hre) => {
+    await hre.run('compile');
 
-  if (hre.network.name === 'mainnet') {
-    console.log('Requires manual setup for mainnet deployment');
-    process.exit(1);
-  } else {
-    const [deployer] = await hre.ethers.getSigners();
-    console.log(`\nDeploying an ERC20 to ${hre.network.name} network using account ${deployer.address}...`);
-    const supply = 100000;
-    const Token20 = await hre.ethers.getContractFactory('Token20');
-    const token20 = await Token20.deploy(supply);
-    await token20.deployed();
-    await new Promise(r => setTimeout(r, 10000));
-    await hre.run('verify:verify', { address: token20.address, constructorArguments: [supply] });
+    if (hre.network.name === 'mainnet') {
+      console.log('Requires manual setup for mainnet deployment');
+      process.exit(1);
+    } else {
+      const [deployer] = await hre.ethers.getSigners();
+      console.log(`\nDeploying an ERC20 to ${hre.network.name} network using account ${deployer.address}...`);
+      const supply = 100000;
+      const Token20 = await hre.ethers.getContractFactory('Token20');
+      const token20 = await Token20.deploy(supply);
+      await token20.deployed();
+      await new Promise((r) => setTimeout(r, 10000));
+      await hre.run('verify:verify', { address: token20.address, constructorArguments: [supply] });
 
-    const outFile = './addresses.json';
-    const addresses = fs.existsSync(outFile) ? require(outFile) : {};
-    addresses[hre.network.name]['erc20token'] = token20.address;
-    fs.writeFileSync(outFile, JSON.stringify(addresses, null, 2));
-  }
-});
+      const outFile = './addresses.json';
+      const addresses = fs.existsSync(outFile) ? require(outFile) : {};
+      addresses[hre.network.name]['erc20token'] = token20.address;
+      fs.writeFileSync(outFile, JSON.stringify(addresses, null, 2));
+    }
+  });
 
 task('upgrade', 'upgrade existing avn-bridge contract')
   .addParam('proxy', 'existing AVN Bridge proxy address')
@@ -148,28 +145,29 @@ task('upgrade', 'upgrade existing avn-bridge contract')
     const AVNBridge = await ethers.getContractFactory('AVNBridge');
     await upgrades.upgradeProxy(args.proxy, AVNBridge);
     console.log(`\nCost: ${hre.ethers.utils.formatEther(balanceBefore.sub(await upgrader.getBalance()))} ETH`);
-    await new Promise(r => setTimeout(r, 30000));
+    await new Promise((r) => setTimeout(r, 30000));
     try {
       const implementationAddress = await hre.upgrades.erc1967.getImplementationAddress(args.proxy);
       await hre.run('verify', { address: implementationAddress });
-    } catch (e) {}
+    } catch (e) {};
   });
 
-task('prepare-upgrade', 'prepare the openzeppelin mainfest (if required)')
-  .addParam('proxy', 'existing AVN Bridge proxy address')
-  .setAction(async (args, hre) => {
-    if (hre.network.name !== 'goerli') return;
-    await hre.run('compile');
-    const AVNBridge = await ethers.getContractFactory('AVNBridge');
-    await upgrades.forceImport(args.proxy, AVNBridge);
-    console.log('Done');
-  });
+  task('prepare-upgrade', 'prepare the openzeppelin mainfest (if required)')
+    .addParam('proxy', 'existing AVN Bridge proxy address')
+    .setAction(async (args, hre) => {
+      if (hre.network.name !== 'goerli') return;
+      await hre.run('compile');
+      const AVNBridge = await ethers.getContractFactory('AVNBridge');
+      await upgrades.forceImport(args.proxy, AVNBridge);
+      console.log('Done');
+    });
 
 function getWeb3Url(networkName) {
   if (!process.env.WEB3_URL_OVERRIDE) {
     return `https://${networkName}.infura.io/v3/${process.env.INFURA_API_KEY || INFURA_API_KEY}`;
-  } else {
-    return process.env.WEB3_URL_OVERRIDE;
+  }
+  else {
+    return process.env.WEB3_URL_OVERRIDE
   }
 }
 module.exports = {
@@ -179,16 +177,16 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: '0.8.21',
+        version: '0.8.17',
         settings: {
           optimizer: {
             enabled: true,
-            runs: 2000,
-            details: { yul: true }
+            runs: 200,
+            details: { yul: false }
           }
         }
       }
-    ]
+    ],
   },
   networks: {
     goerli: {
@@ -196,9 +194,6 @@ module.exports = {
       accounts: [process.env.GOERLI_PRIVATE_KEY || GOERLI_PRIVATE_KEY]
     },
     hardhat: {
-      accounts: {
-        accountsBalance: '5000000000000000000000000' // 5,000,000 ETH TO COVER ACCOUNTS FOR COVERAGE
-      }
       // forking: {
       //   url: getWeb3Url(`mainnet`),
       // },
@@ -206,17 +201,13 @@ module.exports = {
     },
     mainnet: {
       url: getWeb3Url(`mainnet`),
-      accounts: [
-        process.env.MAINNET_PRIVATE_KEY ||
-          MAINNET_PRIVATE_KEY ||
-          '0000000000000000000000000000000000000000000000000000000000000000'
-      ]
+      accounts: [process.env.MAINNET_PRIVATE_KEY || MAINNET_PRIVATE_KEY || "0000000000000000000000000000000000000000000000000000000000000000"]
     }
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY || ETHERSCAN_API_KEY
   },
   gasReporter: {
-    enabled: true
-  }
+   enabled: true,
+ }
 };
