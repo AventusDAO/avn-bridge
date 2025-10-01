a8ef426449270007781a92bd7cd1200601bb0f30
fluorostani <glyn.dimond@aventus.io>
2025-10-01 16:14:18 +0100

    fix coverage

--- test/AvN_Bridge_owner_tests.js
+++ test/AvN_Bridge_owner_tests.js
@@ -4,7 +4,6 @@ const { expect } = require('chai');
 let avnBridge, token777, token20;
 let accounts, authors;
 let owner, someOtherAccount, unauthorizedAccount, newTokenOwner;
-let AVNBridge;
 
 describe('Owner Functions', async () => {
   before(async () => {
@@ -80,86 +79,88 @@ describe('Owner Functions', async () => {
     });
   });
 
-  context('Initialization', function () {
-    const numAuthors = 6;
-    let initVals = generateInitVals(numAuthors);
+  context('Initialization', async () => {
+    let initVals = {};
+    const numAuthors = helper.MIN_AUTHORS + 1;
 
-    function generateInitVals(n) {
-      const initArgs = helper.generateInitArgs(n);
-      initVals = { t1Addresses: initArgs[0], t1PubKeysLHS: initArgs[1], t1PubKeysRHS: initArgs[2], t2PubKeys: initArgs[3] };
+    async function deployAndCatchInitError(expectedError) {
+      const initArgs = [initVals.t1Addresses, initVals.t1PubKeysLHS, initVals.t1PubKeysRHS, initVals.t2PubKeys];
+      const AVNBridge = await ethers.getContractFactory('AVNBridge');
+      await expect(upgrades.deployProxy(AVNBridge, initArgs, { kind: 'uups' })).to.be.revertedWithCustomError(AVNBridge, expectedError);
     }
 
-    context('succeeds', function () {
-      it('with the correct arguments', async () => {
-        const newBridge = await helper.deployAVNBridge(numAuthors);
-        
-        for (i = 0; i < numAuthors; i++) {
-          const authorId = i + 1;
-          expect(await newBridge.t1AddressToId(initVals.t1Addresses[i])).to.equal(authorId);
-          expect(await newBridge.t2PubKeyToId(initVals.t2PubKeys[i])).to.equal(authorId);
-          expect(await newBridge.isAuthor(authorId)).to.be.true;
-          expect(await newBridge.authorIsActive(authorId)).to.be.true;
-          expect(await newBridge.idToT1Address(authorId)).to.equal(initVals.t1Addresses[i]);
-          expect(await newBridge.idToT2PubKey(authorId)).to.equal(initVals.t2PubKeys[i]);
-        }
+    beforeEach(async () => {
+      initVals = { t1Addresses: [], t1PubKeysLHS: [], t1PubKeysRHS: [], t2PubKeys: [] };
 
-        expect(await newBridge.numActiveAuthors()).to.equal(numAuthors);
-        expect(await newBridge.nextAuthorId()).to.equal(numAuthors + 1);
-      });
+      for (i = 0; i < numAuthors; i++) {
+        initVals.t1Addresses.push(authors[i].t1Address);
+        initVals.t1PubKeysLHS.push(authors[i].t1PubKeyLHS);
+        initVals.t1PubKeysRHS.push(authors[i].t1PubKeyRHS);
+        initVals.t2PubKeys.push(authors[i].t2PubKey);
+      }
     });
 
-    context('fails', function () {
-      async function deployAndCatchInitError(expectedError) {
-        const initArgs = [initVals.t1Addresses, initVals.t1PubKeysLHS, initVals.t1PubKeysRHS, initVals.t2PubKeys];
-        const AVNBridge = await ethers.getContractFactory('AVNBridge');
-        await expect(upgrades.deployProxy(AVNBridge, initArgs, { kind: 'uups' })).to.be.revertedWithCustomError(AVNBridge, expectedError);
+    it('succeeds', async () => {
+      const initArgs = [initVals.t1Addresses, initVals.t1PubKeysLHS, initVals.t1PubKeysRHS, initVals.t2PubKeys];
+      const newBridge = await upgrades.deployProxy(AVNBridge, initArgs, { kind: 'uups' });
+
+      for (i = 0; i < numAuthors; i++) {
+        const authorId = i + 1;
+        expect(await newBridge.t1AddressToId(initVals.t1Addresses[i])).to.equal(authorId);
+        expect(await newBridge.t2PubKeyToId(initVals.t2PubKeys[i])).to.equal(authorId);
+        expect(await newBridge.isAuthor(authorId)).to.equal(true);
+        expect(await newBridge.authorIsActive(authorId)).to.equal(true);
+        expect(await newBridge.idToT1Address(authorId)).to.equal(initVals.t1Addresses[i]);
+        expect(await newBridge.idToT2PubKey(authorId)).to.equal(initVals.t2PubKeys[i]);
       }
 
-      beforeEach(async () => {
-        generateInitVals(numAuthors);
-      });
+      expect(await newBridge.numActiveAuthors()).to.equal(numAuthors);
+      expect(await newBridge.nextAuthorId()).to.equal(numAuthors + 1);
+    });
 
-      it('when a T1 address does not correspond to its public key', async () => {
-        initVals.t1PubKeysLHS[0] = authors[1].t1PubKeyLHS;
-        await deployAndCatchInitError('AddressMismatch');
-      });
+    it('fails when a T1 address does not correspond to its public key', async () => {
+      initVals.t1PubKeysLHS[0] = authors[numAuthors].t1PubKeyLHS;
+      await deployAndCatchInitError('AddressMismatch');
+    });
 
-      it('when addresses are missing', async () => {
-        initVals.t1Addresses.pop();
-        await deployAndCatchInitError('MissingKeys');
-      });
+    it('when addresses are missing', async () => {
+      initVals.t1Addresses.pop();
+      await deployAndCatchInitError('MissingKeys');
+    });
 
-      it('when t1 key left halves are missing', async () => {
-        initVals.t1PubKeysLHS.pop();
-        await deployAndCatchInitError('MissingKeys');
-      });
+    it('when t1 key left halves are missing', async () => {
+      initVals.t1PubKeysLHS.pop();
+      await deployAndCatchInitError('MissingKeys');
+    });
 
-      it('when t1 key right halves are missing', async () => {
-        initVals.t1PubKeysRHS.pop();
-        await deployAndCatchInitError('MissingKeys');
-      });
+    it('when t1 key right halves are missing', async () => {
+      initVals.t1PubKeysRHS.pop();
+      await deployAndCatchInitError('MissingKeys');
+    });
 
-      it('when t2 keys are missing', async () => {
-        initVals.t2PubKeys.pop();
-        await deployAndCatchInitError('MissingKeys');
-      });
+    it('when t2 keys are missing', async () => {
+      initVals.t2PubKeys.pop();
+      await deployAndCatchInitError('MissingKeys');
+    });
 
-      it('if any t1 addresses are duplicated', async () => {
-        initVals.t1Addresses[0] = initVals.t1Addresses[2];
-        initVals.t1PubKeysLHS[0] = initVals.t1PubKeysLHS[2];
-        initVals.t1PubKeysRHS[0] = initVals.t1PubKeysRHS[2];
-        await deployAndCatchInitError('T1AddressInUse');
-      });
+    it('fails if any t1 addresses are duplicated', async () => {
+      initVals.t1Addresses[0] = initVals.t1Addresses[2];
+      initVals.t1PubKeysLHS[0] = initVals.t1PubKeysLHS[2];
+      initVals.t1PubKeysRHS[0] = initVals.t1PubKeysRHS[2];
+      await deployAndCatchInitError('T1AddressInUse');
+    });
 
-      it('if any t2 addresses are duplicated', async () => {
-        initVals.t2PubKeys[1] = initVals.t2PubKeys[3];
-        await deployAndCatchInitError('T2KeyInUse');
-      });
+    it('fails if any t2 addresses are duplicated', async () => {
+      initVals.t2PubKeys[1] = initVals.t2PubKeys[3];
+      await deployAndCatchInitError('T2KeyInUse');
+    });
 
-      it('when not enough authors are provided', async () => {
-        generateInitVals(2);
-        await deployAndCatchInitError('NotEnoughAuthors');
-      });
+    it('fails when there are too few authors', async () => {
+      initVals.t1Addresses.splice(helper.MIN_AUTHORS - 1);
+      initVals.t1PubKeysLHS.splice(helper.MIN_AUTHORS - 1);
+      initVals.t1PubKeysRHS.splice(helper.MIN_AUTHORS - 1);
+      initVals.t2PubKeys.splice(helper.MIN_AUTHORS - 1);
+      await deployAndCatchInitError('NotEnoughAuthors');
     });
   });
 
