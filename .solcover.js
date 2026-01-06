81f43bc80cb9e81f51f99097e98a35d146af365c
fluorostani <39763263+fluorostani@users.noreply.github.com>
2026-01-06 20:41:05 +0000

    Update .solcover.js

--- .solcover.js
+++ .solcover.js
@@ -1,3 +1,3 @@
 module.exports = {
-  skipFiles: ['AVTAuthority.sol', 'test/AVNBridgeUpgrade.sol', 'test/MockAVT.sol', 'test/Token20.sol', 'test/Token777.sol', 'test/ReentrantToken.sol']
+  skipFiles: ['test/AVNBridgeUpgrade.sol', 'test/MockAVT.sol', 'test/Token20.sol', 'test/Token777.sol', 'test/ReentrantToken.sol']
 };
